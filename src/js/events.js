54684a343ec24585bc7de68070624162f5bfb924
Bluyett <bluyettproject@gmail.com>
2025-04-19 22:18:45 -0600

    Modo oscuro, estilado e interaccion

--- src/js/events.js
+++ src/js/events.js
@@ -1,4 +1,9 @@
-export {display_menu};
+export {display_menu, change_theme, initializeTheme};
+
+function initializeTheme() {
+    const savedTheme = localStorage.getItem('theme') || 'light';
+    document.documentElement.setAttribute('data-theme', savedTheme);
+}
 
 function display_menu(menu_button, menu_icon, mobile_hamburger_menu, transparent_block, message_container, higher_section, footer){
     if (mobile_hamburger_menu.classList.contains("active")){
@@ -10,12 +15,19 @@ function display_menu(menu_button, menu_icon, mobile_hamburger_menu, transparent
         higher_section.classList.remove("disabled");
         footer.classList.remove("menu-hamburger-displayed");
     } else {
-        mobile_hamburger_menu.classList.toggle("active");
+        mobile_hamburger_menu.classList.add("active");
         menu_icon.setAttribute("src", "../assets/Icons/xmark-solid.svg");
         menu_button.setAttribute('aria-expanded', 'true');
-        transparent_block.classList.toggle("visible");
-        message_container.classList.toggle("disabled");
-        higher_section.classList.toggle("disabled");
-        footer.classList.toggle("menu-hamburger-displayed");
+        transparent_block.classList.add("visible");
+        message_container.classList.add("disabled");
+        higher_section.classList.add("disabled");
+        footer.classList.add("menu-hamburger-displayed");
     }
 };
+
+function change_theme(){
+    const current_theme = document.documentElement.getAttribute('data-theme');
+    const new_theme = current_theme === "light" ? "dark" : "light"
+    document.documentElement.setAttribute('data-theme', new_theme);
+    localStorage.setItem('theme', new_theme)
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,19 @@
-import { display_menu} from "./events.js";
+import { display_menu, change_theme, initializeTheme} from "./events.js";
 
-const menu_button = document.querySelector(".menu-button");
-const mobile_hamburger_menu = document.querySelector(".mobile-hamburger-menu");
-const transparent_block = document.querySelector(".transparent-block");
-const footer = document.querySelector("footer");
-const message_container = document.querySelector(".message-container");
-const higher_section = document.querySelector(".higher-section");
-const menu_icon = document.querySelector(".menu-icon")
 
 document.addEventListener("DOMContentLoaded", () => {
+
+    initializeTheme()
+
+    const menu_button = document.querySelector(".menu-button");
+    const mobile_hamburger_menu = document.querySelector(".mobile-hamburger-menu");
+    const transparent_block = document.querySelector(".transparent-block");
+    const footer = document.querySelector("footer");
+    const message_container = document.querySelector(".message-container");
+    const higher_section = document.querySelector(".higher-section");
+    const menu_icon = document.querySelector(".menu-icon");
+    const dark_mode_button = document.querySelector(".dark-mode");
+
     menu_button.addEventListener("click", () => display_menu(menu_button, menu_icon, mobile_hamburger_menu, transparent_block, message_container, higher_section, footer));
+    dark_mode_button.addEventListener("click", () => change_theme());
 })
